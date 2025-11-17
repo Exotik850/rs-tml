fc88cdc0a2dbe5c3a7cc22c426ea1e912aceb9a5
kidkool850@gmail.com <kidkool850@gmail.com>
2025-11-16 23:15:56 -0600

    add document struct and tests

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,7 @@
 mod attribute;
 pub use attribute::Attribute;
+mod document;
+pub use document::Document;
 mod element;
 pub use element::{Element, Node, Text};
 mod tag;
@@ -12,3 +14,42 @@ mod util;
 #[cfg(test)]
 pub(crate) use util::test_util;
 pub(crate) use util::{nested, quote_nested};
+
+#[cfg(test)]
+mod tests {
+    use crate::RSTMLParseExt;
+
+    #[test]
+    fn test_large_document() {
+        let input = r#"
+            div {
+               // specific styles can be added like so
+               // only one style block per element
+               style {
+                  .padding = "1rem"
+                  .background-color = "blue"
+               }
+
+               #main // id shorthand
+
+               .class = "bg-blue"
+               .lg // class shorthand
+
+               h1 { "Something" }
+               h2 {
+              	.class = "bg-red"
+                    "Something red"
+               }
+
+               a { .href = "https://google.com" "Links should work" }
+
+               br {} // self closing / empty tags
+
+               // comments should work
+               /* multi
+                  line
+                  comments should too */
+                  "#;
+        assert!(crate::Document::parse_ignoring_comments(input).is_ok());
+    }
+}
